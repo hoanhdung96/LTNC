7b27313a47b52ee0b1ec7ac8f7319195a78e50eb
Hoanhdung <25023194@vnu.edu.vn>
2026-05-04 08:05:38 +0700

    Introduce bug for debugging test

--- Bai1/src/test/java/MathUtilsTest.java
+++ Bai1/src/test/java/MathUtilsTest.java
@@ -5,6 +5,6 @@ class MathUtilsTest {
     @Test
     void testAdd() {
         MathUtils utils = new MathUtils();
-        assertEquals(5, utils.add(2, 3), "2 + 3 phải bằng 5");
+        assertEquals(10, utils.add(2, 3), "2 + 3 phải bằng 5");
     }
 }
